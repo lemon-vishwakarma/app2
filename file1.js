cf533847b22340a6081c53d2f907c2da33598f2e
Rahul Vishwakarma <rahulmkv1997@gmail.com>
2022-06-20 11:23:07 +0530

    new changes

--- file1.js
+++ file1.js
@@ -1,3 +1,4 @@
+// ODD-NUMBER BRANCH
 const isOdd = (number) => {
     if (number % 2 != 0) {
         console.log(`${number} = ODD`);
@@ -8,3 +9,16 @@ const isOdd = (number) => {
 
 isOdd(10);
 isOdd(11);
+
+// EVEN-NUMBER BRANCH
+const isEven = (number) => {
+    if (number % 2 == 0) {
+        console.log(`${number} = EVEN`);
+    } else {
+        console.log(`${number} = NOT EVEN`);
+    }
+}
+
+isEven(10);
+isEven(11);
+
